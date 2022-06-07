6307245446d9b9a39d112caf348846fedf55fd74
imhalid <imhalid@icloud.com>
2022-06-07 19:57:20 +0300

    abstrat-help-page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="layout">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,54 @@
+export const data = [
+  {
+    id: 1,
+    title: "Using Abstract",
+    describtion:
+      "Abstract lets you manage, version, and document your designs in one place.",
+    image:
+      "https://theme.zdassets.com/theme_assets/2197739/aed6500f4da41c641f3de5d96247eb6654e9c386.png",
+    url: "https://help.abstract.com/hc/en-us/categories/360005436612-Using-Abstract",
+  },
+  {
+    id: 2,
+    title: "Manage your account",
+    describtion:
+      "Configure your account settings, such as your email, profile details, and password.",
+    image:
+      "https://theme.zdassets.com/theme_assets/2197739/91588b3d042df901d9b941ccbb57af4f151401c7.png",
+    url: "https://help.abstract.com/hc/en-us/categories/360005449431-Manage-your-account",
+  },
+  {
+    id: 3,
+    title: "Manage organizations, teams, and projects",
+    describtion:
+      "Use Abstract organizations, teams, and projects to organize your people and your work.",
+    image:
+      "https://theme.zdassets.com/theme_assets/2197739/50d0db451c0e1e66e1caddd142986fcf6792ca05.png",
+    url: "https://help.abstract.com/hc/en-us/categories/360005449531-Manage-organizations-teams-and-projects",
+  },
+  {
+    id: 4,
+    title: "Manage billing",
+    describtion: "Change subscriptions and payment details.",
+    image:
+      "https://theme.zdassets.com/theme_assets/2197739/3e82d0126e97d7b7c60a611d4e11b3c278d33743.png",
+    url: "https://help.abstract.com/hc/en-us/categories/360005436672-Manage-billing",
+  },
+  {
+    id: 5,
+    title: "Authenticate to Abstract",
+    describtion:
+      "Set up and configure SSO, SCIM, and Just-in-Time provisioning.",
+    image:
+      "https://theme.zdassets.com/theme_assets/2197739/7712592465ef5c7bf18f1c7cb16e19ab44eb81e3.png",
+    url: "https://help.abstract.com/hc/en-us/categories/360005436592-Authenticate-to-Abstract",
+  },
+  {
+    id: 6,
+    title: "Abstract support",
+    describtion: "Get in touch with a human.",
+    image:
+      "https://theme.zdassets.com/theme_assets/2197739/dcc25d18e224d12621d74e545cc8bdd1b5685ca8.png",
+    url: "https://help.abstract.com/hc/en-us/articles/360050382291-Contact-Support",
+  },
+];
