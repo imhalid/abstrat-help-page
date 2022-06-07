1232b148b733f114a72781dceef8486a7436e0db
imhalid <imhalid@icloud.com>
2022-06-08 00:10:38 +0300

    day1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="layout">
       <Header />
+      <Search />
     </div>
   );
 }
---
+++ src/Search.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Search.css";
+
+const Search = () => {
+  return (
+    <section className="search-container">
+      <div className="container-inner">
+        <h1 className="search-title">How can we help?</h1>
+        <form>
+          <input className="search-box" type="text" placeholder="Search" />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Search;
