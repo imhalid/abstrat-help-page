0b9f888f0a8ef599044262bae954a44eef7bc3e0
imhalid <imhalid@icloud.com>
2022-06-12 19:40:10 +0300

    add grid

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,26 +1,29 @@
 import React from "react";
 import { data } from "./data";
+import "./Content.css";
 
 const Content = () => {
   return (
-    <>
-      {data.map((item, key) => (
-        <section key={key} className="content-container">
-          <img
-            src={item.image}
-            alt={item.image.length}
-            className="content-logo"
-          ></img>
-          <div className="content-texts">
-            <div className="content-title">{item.title}</div>
-            <div className="content-description">{item.describtion}</div>
-            <a href={item.url} className="content-link">
-              Link
-            </a>
-          </div>
-        </section>
-      ))}
-    </>
+    <div className="content-inner">
+      <div className="content">
+        {data.map((item, key) => (
+          <section key={key} className="content-container">
+            <img
+              src={item.image}
+              alt={item.image.length}
+              className="content-logo"
+            ></img>
+            <div className="content-texts">
+              <h4 className="content-title">{item.title}</h4>
+              <div className="content-description">{item.describtion}</div>
+              <a href={item.url} className="content-link">
+                Learn More →
+              </a>
+            </div>
+          </section>
+        ))}
+      </div>
+    </div>
   );
 };
 
